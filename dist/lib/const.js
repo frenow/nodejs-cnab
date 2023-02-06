b69b7e117d4deec828c018f31d878ec52104578e
Ricardo Barros <169519@upf.br>
2023-02-06 08:17:28 -0300

    feat: novo build

--- dist/lib/const.js
+++ dist/lib/const.js
@@ -28,7 +28,7 @@ exports.BANK = {
         },
         retorno: {
             400: ['header_arquivo', 'detalhe'],
-            240: ['header_arquivo', 'detalhe_segmento_t', 'detalhe_segmento_u']
+            240: ['header_arquivo', 'header_lote', 'detalhe_segmento_t', 'detalhe_segmento_u']
         }
     },
     banrisul: {
